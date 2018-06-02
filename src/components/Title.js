769d740897761b26d926dd1c5583eb1d07be3431
jocvegar <jocvegar@gmail.com>
2018-06-01 19:30:32 -0500

    finishes weather app

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,12 +10,14 @@ import React from 'react';
 // 	}
 // }
 
-// when returning a single div we can get rid of the return statement!s
+// when returning a single div we can get rid of the return statement!
 
 const Title = () => (
 	<div>
-		<h1>Weather Finder</h1>
-		<p>Please enter a city and a country</p>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<p className="title-container__subtitle">
+			Please enter a city and a country
+		</p>
 	</div>
 );
 
